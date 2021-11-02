5a85fa741fefc548f5533507371eba2157ad627d
Ben Townsend <RBT000011@gmail.com>
2021-11-02 17:35:17 -0500

    changes made to models and api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const BlogRoutes = require("./BlogRoutes");
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use("/users", userRoutes);
+router.use("/Blogs", BlogRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,18 @@
 const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./commentSection");
 
-module.exports = { User };
+Blog.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Blog, Comment };
